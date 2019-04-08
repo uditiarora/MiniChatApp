2e8c2c42d36b492f41f9d456ad341f6c4b55f266
Uditi <uditiarora@gmail.com>
2019-04-08 22:25:47 +0530

    initial commit

---
+++ socket.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+var users = [];
+var connections = [];
+
+server.listen(process.env.PORT || 3000);
+console.log('Server running...');
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.sockets.on('connection', function (socket) {
+    connections.push(socket);
+    console.log('Connected: %s sockets connected.', connections.length);
+
+    // Disconnect
+    socket.on('disconnect', function (data) {
+        users.splice(users.indexOf(socket.username), 1);
+        updateUsernames();
+        connections.splice(connections.indexOf(socket), 1);
+        console.log('Disconnected: %s sockets connected.', connections.length);
+    });
+
+    // Send Message
+    socket.on('send message', function (data) {
+        io.sockets.emit('new message', {msg: data, user: socket.username});
+    });
+
+    // User login
+    socket.on('login', function (data, callback) {
+        callback(true);
+        socket.username = data;
+        users.push(socket.username);
+        updateUsernames();
+    });
+
+    function updateUsernames() {
+        io.sockets.emit('get users', users);
+    }
+});
